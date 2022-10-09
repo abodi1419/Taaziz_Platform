1eebda55918739bfd870b32110b9520281778aa0
Abdullah Alqarni <abdullah4alqarni@gmail.com>
2022-10-09 10:51:06 +0300

    Update - select the missing lines
    Update - select the missing lines and put them in your new file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,18 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <style>
-        .bg-navbar-kau{
-            background: linear-gradient(180deg, rgb(45, 116, 36) 0%, rgb(136, 243, 122) 100%);
-            /*height: 150px;*/
-        }
-        /*body{*/
-        /*    background: linear-gradient(180deg, rgb(136, 243, 122) 0%, rgb(255, 255, 255) 29%);*/
-        /*}*/
-        body{
-            padding-top: 80px;
-        }
-    </style>
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
 
@@ -26,167 +15,176 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-{{--    <link href="{{ mix('css/app.css') }}" rel="stylesheet">--}}
-    <link rel="icon" type="image/x-icon" href="{{url('kau.png')}}" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
     @if(\Illuminate\Support\Facades\App::getLocale()!='ar')
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     @else
-        <link href="{{ asset('css/bootstrap.rtl.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap(rtl).css') }}" rel="stylesheet">
 
     @endif
 
 
-    <!-- Scripts -->
+<!-- Scripts -->
     <script src="{{ asset('js/jquery-3.6.0.js') }}"></script>
-{{--    <script src="{{ mix('js/app.js') }}"></script>--}}
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-
-
-
+    @if(\Illuminate\Support\Facades\App::getLocale()!='ar')
+    <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
+    @else
+        <script src="{{ asset('js/bootstrap.bundle(rtl).js') }}"></script>
+        <script src="{{ asset('js/bootstrap.bundle(rtl).js') }}"></script>
+    @endif
+    <script src="{{asset('js/likes.js')}}"></script>
 </head>
 <body @if(\Illuminate\Support\Facades\App::getLocale()=='ar') dir="rtl" @endif>
     <div id="app">
-{{--        Navbar start        --}}
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top p-3">
-            <div class="container-fluid">
-
-
-                <a class="navbar-brand text-success" href="{{url('/')}}">
-
-                    <h2 class="d-inline"><img src="{{url('logo1.png')}}" alt=""></h2>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ __(config('app.name', 'Laravel')) }}
                 </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-                <ul class="navbar-nav flex-grow-1 flex-md-grow-0 px-2">
-                    @if(\Illuminate\Support\Facades\App::getLocale()=='en')
-                        <li class="nav-item text-end">
-                            <a class="nav-link" href="{{ url('locale/ar') }}">عربي</a>
-                        </li>
-                    @else
-                        <li class="nav-item text-end">
-                            <a class="nav-link" href="{{ url('locale/en') }}">{{ __('EN') }}</a>
-                        </li>
-                    @endif
-                </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    @auth
+                    <ul class="navbar-nav me-auto">
+                        @can("Role list")
+                            <li class="nav-item dropdown">
+                                <a id="roleDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ __("Roles") }}
+                                </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><a class="navbar-brand" href="{{url('/')}}">{{ __(config('app.name', 'Laravel')) }}</a></h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-
-                        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
-                            @auth()
-                                <div class="btn-group w-100 h6 p-0 m-0 d-md-none">
-                                    <a class="nav-link w-100 dropdown-toggle h3" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                                        {{ Auth::user()->name }}
-
-                                        <img src="{{asset(Auth::user()->image)}}" class="rounded-circle" alt="User image" height="20" width="20">
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-end">
-                                        <li>
-                                            <a class="dropdown-item" href="{{ route('student.index') }}">
-                                                {{ Auth::user()->name }}
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="dropdown-item" href="{{ route('logout') }}">
-
-                                                {{ __('Logout') }}
-                                            </a>
-                                        </li>
-                                    </ul>
+                                <div class="dropdown-menu dropdown-menu-left" aria-labelledby="roleDropdown">
+                                    <a class="dropdown-item" href="{{route('roles.index')}}">{{__("View roles")}}</a>
+                                    <a class="dropdown-item" href="{{route('roles.create')}}">{{__("Create role")}}</a>
                                 </div>
-                                @if(auth()->user()->hasRole('student'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{route('profile.index')}}" role="button">
-                                            {{ __("Profile") }}
-                                        </a>
-                                    </li>
-                                @endif
-                                @can("User list")
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ __("Users") }}
-                                        </a>
+                            </li>
+                        @endcan
+                        @can("User list")
+                            <li class="nav-item dropdown">
+                                <a id="roleDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ __("Users") }}
+                                </a>
 
-                                        <div class="dropdown-menu dropdown-menu-left" aria-labelledby="roleDropdown">
-                                            <a class="dropdown-item" href="{{route('users.index')}}">{{__("View users")}}</a>
-                                            <a class="dropdown-item" href="{{route('users.create')}}">{{__("Create user")}}</a>
-                                        </div>
-                                    </li>
-                                @endcan
-                                @can("Role list")
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ __("Roles") }}
-                                        </a>
+                                <div class="dropdown-menu dropdown-menu-left" aria-labelledby="roleDropdown">
+                                    <a class="dropdown-item" href="{{route('users.index')}}">{{__("View users")}}</a>
+                                    <a class="dropdown-item" href="{{route('users.create')}}">{{__("Create user")}}</a>
 
-                                        <div class="dropdown-menu dropdown-menu-left" aria-labelledby="roleDropdown">
-                                            <a class="dropdown-item" href="{{route('roles.index')}}">{{__("View roles")}}</a>
-                                            <a class="dropdown-item" href="{{route('roles.create')}}">{{__("Create role")}}</a>
-                                        </div>
-                                    </li>
-                                @endcan
-                            @endauth
-
-                        </ul>
-                        <ul class="navbar-nav justify-content-end pe-3">
-                            @auth()
-
-                                <li class="nav-item d-none d-md-inline">
-
-                                    <div class="btn-group w-100 h6 p-0 m-0">
-                                        <a class="nav-link w-100 dropdown-toggle h3" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                                            {{ Auth::user()->name }}
-
-                                            <img src="{{asset(Auth::user()->image)}}" class="rounded-circle" alt="User image" height="20" width="20">
-                                        </a>
-                                        <ul class="dropdown-menu dropdown-menu-end">
-                                            <li>
-                                                <a class="dropdown-item" href="{{ route('student.index') }}">
-                                                    {{ Auth::user()->name }}
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a class="dropdown-item" href="{{ route('logout') }}">
-
-                                                        {{ __('Logout') }}
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-
-
+                                </div>
+                            </li>
+                        @endcan
+                            @can("Dashboard list")
+                                <li class="nav-item dropdown">
+                                    <a id="roleDropdown" class="nav-link dropdown-toggle" href="{{route('Dashboard.index')}}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ __("Dashboard") }}
+                                    </a>
                                 </li>
-                            @else
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                @endif
+                            @endcan
+                        @if(auth()->user()->hasRole('student'))
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="{{route('profile.index')}}" role="button">
+                                    {{ __("Profile") }}
+                                </a>
 
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif
 
-                            @endauth
-                        </ul>
-                    </div>
+                            </li>
+                        @endif
+
+                    </ul>
+                    @endauth
+
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('student.index') }}">
+                                        {{ Auth::user()->name }}
+                                    </a>
+
+
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+
+                                    {{--           Articles             --}}
+                            <li class="nav-item">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{__('Community')}}
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('myArticles') }}">
+                                         {{ __('my Posts') }}
+                                    </a>
+
+
+                                    <a class="dropdown-item" href="{{ route('articles.index') }}">
+                                        {{ __('All Posts') }}
+                                    </a>
+                                </div>
+                            </li>
+                            {{--           Jobs             --}}
+                            <li class="nav-item">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{__('Jobs')}}
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('jobs.index') }}">
+                                        {{ __('Jobs from college') }}
+                                    </a>
+
+
+                                    <a class="dropdown-item" href="">
+                                        {{ __('smart search') }}
+                                    </a>
+                                </div>
+                            </li>
+
+                        @endguest
+                        @if(\Illuminate\Support\Facades\App::getLocale()=='en')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('locale/ar') }}">عربي</a>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('locale/en') }}">{{ __('English') }}</a>
+                            </li>
+                        @endif
+                    </ul>
                 </div>
-
             </div>
-
         </nav>
-{{--        Navbar end      --}}
-        <main class="py-4 pt-5">
+
+        <main class="py-4">
             @if($errors->any())
                 <div class="alert alert-danger">
                     <ul>
